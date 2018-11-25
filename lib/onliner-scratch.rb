61a06481e5b192bc60fe15ddbb507c60b8de1f5f
Eugene <q010@bk.ru>
2018-11-26 00:55:46 +0300

    refactoring 2. classes

--- lib/onliner-scratch.rb
+++ lib/onliner-scratch.rb
@@ -3,36 +3,69 @@ require 'capybara'
 require 'RSpec'
 require 'csv'
 
-# Configurations
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-Capybara.javascript_driver = :chrome
-Capybara.configure do |config|
-  config.default_max_wait_time = 10 # seconds
-  config.default_driver = :selenium
-end
-browser = Capybara.current_session
-driver = browser.driver.browser
-browser.visit "https://www.onliner.by/"
-id_array = browser.execute_script(
-    " var m = document.getElementsByClassName('b-tile');
+class Scratcher
+  def initialize
+    Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
+    end
+    Capybara.javascript_driver = :chrome
+    Capybara.configure do |config|
+      config.default_max_wait_time = 10 # seconds
+      config.default_driver = :selenium
+    end
+    @browser = Capybara.current_session
+    driver = @browser.driver.browser
+  end
+  def go_to_main_page
+    @browser.visit "https://www.onliner.by/"
+  end
+  def get_ids_in_class(class_name)
+    @browser.execute_script(
+        " var m = document.getElementsByClassName('#{class_name}');
             var t = []
 
             for (index = 0; index < m.length; ++index) {
               t.push(m[index].id);
             }
-            return t;
-    ",
-).find_all{ |elem| elem != "" }
-CSV.open("data.csv", "w", encoding: "UTF-8") do |wr|
-  id_array.each do |id|
-    browser.find('#' + id).click
-    wr << [
-            browser.find('.news-header__title > div').text,
-            browser.find('.news-header__image').native.css_value('background-image').slice(5..-3),
-            browser.find('.news-entry__speech').text.slice(0..199)
-          ]
-    browser.visit 'https://www.onliner.by/'
+            return t;",
+        ).find_all{ |elem| elem != "" }
+  end
+  def get_all_news(id_list)
+    news = []
+    id_list.each do |id|
+      self.go_to_page(id)
+      begin
+      news << [
+            @browser.find('.news-header__title > div').text,
+            @browser.find('.news-header__image').native.css_value('background-image').slice(5..-3),
+            @browser.find('.news-entry__speech').text.slice(0..199)
+      ]
+      rescue
+        puts 'oops'
+      end
+      self.go_to_main_page
+    end
+    news
+  end
+  def go_to_page(id)
+    @browser.find('#' + id).click
   end
 end
+
+class CSVwriter
+  def initialize(filename)
+    @filename = filename
+  end
+  def write_info(info)
+    CSV.open(@filename, "w", encoding: "UTF-8") do |wr|
+      info.each do |inf_block|
+        wr << inf_block
+      end
+    end
+  end
+end
+
+scratcher = Scratcher.new
+scratcher.go_to_main_page
+CSVwriter.new("data.csv").write_info(scratcher.get_all_news(scratcher.get_ids_in_class("b-tile"))
+)
--- spec/tests.rb
+++ spec/tests.rb
@@ -1,13 +1,12 @@
 require 'RSpec'
 require '../lib/array_mover'
-
-
 RSpec.describe Array_mover do
+  let(:array) { [1,2,3] }
   it 'returns same' do
-    expect(Array_mover.move([1,2,3], 3)).to eq([1,2,3])
+    expect(Array_mover.move(array, 3)).to eq([1,2,3])
   end
   it 'returns one left move' do
-    expect(Array_mover.move([1,2,3], [1,2,3].length-1)).to eq([2,3,1])
+    expect(Array_mover.move(array, array.length-1)).to eq([2,3,1])
   end
   it 'returns array of 1 el' do
     expect(Array_mover.move([1], 5)).to eq([1])
