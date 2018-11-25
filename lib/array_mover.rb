ce853253a740ad4f58e0c2b80f800a8d42e65e78
Eugene <q010@bk.ru>
2018-11-25 17:01:21 +0300

    refactirong

--- lib/array_mover.rb
+++ lib/array_mover.rb
@@ -1,14 +1,5 @@
 class Array_mover
   def self.move(list, position)
-    position.times do
-      buffer = list[list.size - 1]
-      index = list.size - 1
-      (list.length - 1).times do
-        list[index] = list[index-1]
-        index -= 1
-      end
-      list[0] = buffer
-    end
-    list
+    list.concat(list.shift((list.length - position).abs))
   end
 end
--- spec/tests.rb
+++ spec/tests.rb
@@ -7,9 +7,12 @@ RSpec.describe Array_mover do
     expect(Array_mover.move([1,2,3], 3)).to eq([1,2,3])
   end
   it 'returns one left move' do
-    expect(Array_mover.move([1,2,3], [1,2,3].length-1)).to eq([3,3,1]) #fail here
+    expect(Array_mover.move([1,2,3], [1,2,3].length-1)).to eq([2,3,1])
   end
   it 'returns array of 1 el' do
     expect(Array_mover.move([1], 5)).to eq([1])
+  end
+  it 'returns 3 moves' do
+    expect(Array_mover.move([1,2,3,4,5], 8)).to eq([4,5,1,2,3])
   end
 end
